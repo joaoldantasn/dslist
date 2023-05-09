b39c8ac9a068d8892a0ed6158026255ecec56694
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-05-09 13:46:44 -0300

    project created

---
+++ src/test/java/com/jlucas/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlucas.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
